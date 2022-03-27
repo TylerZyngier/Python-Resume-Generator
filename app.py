8d62d008870e0919b14f82139ad4e46121e9b353
Tyler Zyngier <tyler.zyngier@gmail.com>
2022-03-27 03:13:04 -0400

    Inital Commit

---
+++ app.py
@@ -0,0 +1,92 @@
+# BEFORE RUNNING APP MAKE SURE TO RUN COMMAND: pip install -r requirements.txt
+
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+# add profile picture
+document.add_picture(
+    'Images/ProfilePicture.png',
+    width=Inches(1.5)
+)
+
+# name, phone number, and email details
+name = input('Your name: ')
+phone_number = input('Your phone number: ')
+email = input('Your email: ')
+
+document.add_paragraph(
+    name + '\n' + phone_number + '\n' + email)
+
+# about me
+document.add_heading('About Me')
+document.add_paragraph(
+    input('Tell me about yourself: ')
+)
+
+# work experience
+document.add_heading("Work Experience")
+
+
+def add_experience():
+    p = document.add_paragraph()
+
+    company = input('\nEnter a company you have worked for: ')
+    from_date = input('From date: ')
+    to_date = input('To date: ')
+
+    p.add_run(company + ' ').bold = True
+    p.add_run(from_date + '-' + to_date + '\n').italic = True
+
+    experience_details = input(
+        'Describe your experience at ' + company + ': ')
+    p.add_run(experience_details)
+
+
+add_experience()
+
+while True:
+    has_more_experience = input(
+        'Do you have more work experience? (Yes or No) ')
+
+    if has_more_experience.lower() == 'yes':
+        add_experience()
+    elif has_more_experience.lower() == 'no':
+        break
+    else:
+        print('Invalid answer, please input Yes or No')
+        continue
+
+# skills
+document.add_heading('Skills')
+
+
+def add_skill():
+    skill = input('Enter skill: ')
+    p = document.add_paragraph(skill)
+    p.style = 'List Bullet'
+
+
+add_skill()
+
+while True:
+    has_more_skills = input(
+        'Do you have more skills? (Yes or No) ')
+
+    if has_more_skills.lower() == 'yes':
+        add_skill()
+    elif has_more_skills.lower() == 'no':
+        break
+    else:
+        print('Invalid answer, please input Yes or No')
+        continue
+
+# footer
+section = document.sections[0]
+footer = section.footer
+p = footer.paragraphs[0]
+p.text = "Resume generated with Python code"
+
+# save the document
+document.save('resume.docx')
